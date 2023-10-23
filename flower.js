ea951c4d6b863c3e5acfb433778d1280418ea72c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-10-23 09:57:16 +0545

    DAY 4

--- flower.js
+++ flower.js
@@ -1,8 +1,6 @@
-
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
